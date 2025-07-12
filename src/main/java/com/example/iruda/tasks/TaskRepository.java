9190c16ce03c9f77ec842883a4118f736ade8a53
kimminchae <0720kmc@naver.com>
2025-07-12 21:39:22 +0900

    20250712 알람 코드 수정중

--- src/main/java/com/example/iruda/tasks/TaskRepository.java
+++ src/main/java/com/example/iruda/tasks/TaskRepository.java
@@ -10,11 +10,6 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
 
     List<Task> findAllByProjectId(Long projectId);
 
-    @Query("SELECT t FROM Task t " +
-            "JOIN t.project p " +
-            "JOIN p.projectMembers pm " +
-            "WHERE pm.user.id = :userId " +
-            "AND t.endDate = CURRENT_DATE " +
-            "AND t.alarmSet = 'ON'")
+    @Query("SELECT t FROM Task t " + "JOIN t.project p " + "JOIN p.projectMembers pm " + "WHERE pm.user.id = :userId " + "AND t.endDate = CURRENT_DATE " + "AND t.alarmSet = 'ON'")
     List<Task> findTodayAlarmsByUserId(@Param("userId") Long userId);
 }
